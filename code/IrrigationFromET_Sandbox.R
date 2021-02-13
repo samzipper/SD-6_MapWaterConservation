328279b0008c78b1f452972cd1eb3dea43d1d2d4
Sam Zipper <samzipper@ku.edu>
2021-02-13 13:38:45 -0800

    Exploratory analysis of data

--- code/IrrigationFromET_Sandbox.R
+++ code/IrrigationFromET_Sandbox.R
@@ -5,7 +5,11 @@ source(file.path("code", "paths+packages.R"))
 
 ## load data
 # annual ET and wr group summaries (from DataPrep_03_WRgroups_SummarizeAnnualData.R)
-alldata_wrg <- readr::read_csv(file.path("data", "WRgroups_AnnualData.csv"))
+alldata_wrg <- readr::read_csv(file.path("data", "WRgroups_AnnualData.csv")) %>% 
+  dplyr::mutate(ET_mm = ET_m3/area_m2_wrg,
+                ET_m3_surplus = ET_m3 - ET_m3_nonirr,
+                ET_mm_surplus = ET_m3_surplus/area_m2_wrg) %>% 
+  dplyr::left_join(crop_names.groups, by = c("CropCode_maincrop" = "CropCode"))
 et_fields_yr <- readr::read_csv(file.path("data", "Fields_AnnualET.csv"))
 
 # land cover and irrigation status (from DataPrep_01_Fields_SeparateBoundaries+Attributes.R)
@@ -53,13 +57,63 @@ alldata_fields %>%
   labs(title = "ET rates for dominant crops (LEMA and buffer area)") +
   theme(legend.position = "bottom",
         axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
-  ggsave(file.path("plots", "IrrigationFromET_ETrateBoxplots.png"),
+  ggsave(file.path("plots", "IrrigationFromET_ETrateIrrNonirr.png"),
          width = 190, height = 190, units = "mm")
 
-## add non-irrigated ET to WRG data
-wrg_with_et <-
-  alldata_wrg %>% 
-  dplyr::left_join(crop_names.groups, by = c("CropCode_maincrop" = "CropCode")) %>% 
-  dplyr::left_join(et_nonirr, by = "CropGroup")
+## plot witihn vs outside LEMA ET rates
+alldata_fields %>% 
+  subset(UID %in% UIDs_keep & CropGroup %in% CropGroup_keep & Irrigation == 1) %>% 
+  ggplot(aes(x = Algorithm, y = ET_mm, fill = factor(within_lema))) +
+  geom_boxplot() +
+  facet_grid(Year ~ CropGroup) +
+  scale_x_discrete(name = "ET Algorithm") +
+  scale_y_continuous(name = "Mean ET [mm]") +
+  scale_fill_manual(name = "Location", 
+                    values = c("FALSE" = col.cat.red, "TRUE" = col.cat.blu),
+                    labels = c("FALSE" = "Buffer", "TRUE" = "LEMA")) +
+  labs(title = "Field-resolution irrigated ET rates for dominant crops") +
+  theme(legend.position = "bottom",
+        axis.text.x = element_text(angle = 45, vjust = 1, hjust = 1)) +
+  ggsave(file.path("plots", "IrrigationFromET_IrrigatedETrateByCrop.png"),
+         width = 190, height = 190, units = "mm")
 
-  
+## compare by water rights group
+# entirely in LEMA or buffer
+wrg_lema <- unique(alldata_wrg$WR_GROUP[alldata_wrg$n_within_lema == alldata_wrg$n_irrfields])
+wrg_buffer <- unique(alldata_wrg$WR_GROUP[alldata_wrg$n_within_buffer == alldata_wrg$n_irrfields])
+
+# comparable area between WIMAS and inferred groups
+alldata_wrg$area_diff_prc <- (alldata_wrg$IrrArea_m2 - alldata_wrg$area_m2_wrg)/alldata_wrg$IrrArea_m2
+
+# crop types that are common in both irrigated and non-irrigated land
+CropGroup_keep <- c("Corn", "Sorghum", "Soybeans")  
+
+
+trimmed_wrg <- 
+  alldata_wrg %>% 
+  subset(WR_GROUP %in% c(wrg_lema, wrg_buffer)) %>% 
+  subset(CropGroup %in% CropGroup_keep) %>% 
+  subset(abs(area_diff_prc) > 0.2) %>% 
+  subset(area_m2_maincrop/area_m2_wrg > 0.8)
+
+ggplot(trimmed_wrg, aes(x = Irrigation_m3, y = ET_m3_surplus,
+                        color = CropGroup)) +
+  geom_abline(intercept = 0, slope = 1, color = col.gray) +
+  geom_point() +
+  scale_color_manual(values = pal_crops)
+
+ggplot(trimmed_wrg, aes(x = CropGroup, y = ET_mm_surplus,
+                        fill = (WR_GROUP %in% wrg_lema))) +
+  geom_hline(yintercept = 0, color = col.gray) +
+  geom_boxplot() +
+  facet_wrap(~ Algorithm, scales = "free") +
+  scale_x_discrete(name = "Main Crop Type") +
+  scale_y_continuous(name = "Annual ET Surplus [mm]") +
+  scale_fill_manual(name = "Location", 
+                    values = c("FALSE" = col.cat.red, "TRUE" = col.cat.blu),
+                    labels = c("FALSE" = "Buffer", "TRUE" = "LEMA")) +
+  labs(title = "ET surplus (actual ET - median nonirrigated ET for that crop)",
+       subtitle = "2016-2018 data, trimmed water rights groups") +
+  theme(legend.position = "bottom") +
+  ggsave(file.path("plots", "IrrigationFromET_ETsurplusByAlgorithm.png"),
+         width = 190, height = 120, units = "mm")
