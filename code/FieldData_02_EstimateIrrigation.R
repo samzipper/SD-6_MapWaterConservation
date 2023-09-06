9ac0347102d2e465c1b51e572d8a82b43d1706e0
Sam Zipper <samzipper@ku.edu>
2023-09-06 16:50:34 -0500

    Add some new plots

--- code/FieldData_02_EstimateIrrigation.R
+++ code/FieldData_02_EstimateIrrigation.R
@@ -7,27 +7,30 @@ source(file.path("code", "paths+packages.R"))
 dir_data <- "G:/.shortcut-targets-by-id/1fM3-4oKs6lEiTg-VQECNObDmlw9jVdX3/EGGS/NASA OpenET/data/field-specific water use"
 
 # load all fields and remove data you don't want
-df_all <- read_csv(file.path(dir_data, "FieldData_AllFieldsCompiled.csv")) |> 
-  mutate(MonthYear = mdy(MonthYear)) |> 
-  subset(!(year(MonthYear) == 2016 & FieldID == "SW1"))  # remove 2016 SW1 - no ET data
+df_all <- read_csv(file.path(dir_data, "FieldData_AllFieldsCompiled.csv"))
 
 # pivot to long form
 df_long <- 
   df_all |> 
-  pivot_longer(c("ensemble", "disalexi", "ptjpl", "eemetric", "geesebal", "sims", "ssebop"),
+  pivot_longer(ends_with("_et_mm"),
                names_to = "Algorithm", values_to = "ET_mm") |> 
-  arrange(FieldID, Algorithm)
+  arrange(FieldID, Algorithm) |> 
+  mutate(Algorithm = str_sub(Algorithm, start = 1, end = -7))
 
 # estimate irrigation as ET-P
 df_long$ET.P_mm <- df_long$ET_mm - df_long$precip_mm
 df_long$irrEst_mm <- ifelse(df_long$ET.P_mm >= 0, df_long$ET.P_mm, 0)
+df_long$P.Irr_mm <- df_long$precip_mm + df_long$irrigation_mm
 
 # calculate cumulative irrigation
 df_long_cumirr <- 
   df_long |> 
   group_by(FieldID, Algorithm) |> 
   reframe(irrigation_mm_cumsum = cumsum(irrigation_mm),
-          irrEst_mm_cumsum = cumsum(irrEst_mm)) |> 
+          irrEst_mm_cumsum = cumsum(irrEst_mm),
+          P.Irr_mm_cumsum = cumsum(P.Irr_mm),
+          ET_mm_cumsum = cumsum(ET_mm),
+          ETo_mm_cumsum = cumsum(ETo_mm)) |> 
   mutate(MonthYear = df_long$MonthYear)
 
 ## monthly plots
@@ -45,17 +48,42 @@ ggplot(df_long, aes(x = MonthYear)) +
   theme(legend.position = "bottom")
 ggsave("plots/FieldData_MonthlyTimeseries.png", width = 150, height = 150, units = "mm")
 
+# plot timeseries including ETo
+ggplot(df_long, aes(x = MonthYear)) +
+  geom_hline(yintercept = 0, color = col.gray) +
+  geom_line(aes(y = ET_mm/25.4, color = Algorithm)) +
+  geom_line(aes(y = ETo_mm/25.4), color = "black", linetype = "dashed") +
+  facet_wrap(~FieldID, ncol = 1) +
+  scale_color_manual(values = pal_algorithms, labels = labs_algorithms) +
+  scale_x_date(name = "Month") +
+  scale_y_continuous(name = "Monthly Flux [in]") +
+  labs(title = "Monthly ET (color lines) and ETo (dashed line) Depths") +
+  theme(legend.position = "bottom")
+ggsave("plots/FieldData_MonthlyTimeseries_ET+ETo.png", width = 150, height = 150, units = "mm")
+
 # plot cumulative irrigation
 ggplot(df_long_cumirr, aes(x = MonthYear)) +
   geom_line(aes(y = irrEst_mm_cumsum/25.4, color = Algorithm)) +
   geom_point(aes(y = irrigation_mm_cumsum/25.4), color = "black", shape = 1) +
-  facet_wrap(~FieldID, ncol = 1) +
+  facet_wrap(~FieldID, ncol = 1, scales = "free_y") +
   scale_color_manual(values = pal_algorithms, labels = labs_algorithms) +
   scale_x_date(name = "Month") +
   scale_y_continuous(name = "Monthly Cumulative Irrigation [in]") +
   labs(title = "Monthly Estimated (color lines) and Reported (black dots) Cumulative Irrigation Depths") +
   theme(legend.position = "bottom")
 ggsave("plots/FieldData_MonthlyCumulativeTimeseries.png", width = 150, height = 150, units = "mm")
+
+# plot cumulative P+Irrigation and ET
+ggplot(df_long_cumirr, aes(x = MonthYear)) +
+  geom_line(aes(y = ET_mm_cumsum/25.4, color = Algorithm)) +
+  geom_point(aes(y = P.Irr_mm_cumsum/25.4), color = "black", shape = 1) +
+  facet_wrap(~FieldID, ncol = 1, scales = "free_y") +
+  scale_color_manual(values = pal_algorithms, labels = labs_algorithms) +
+  scale_x_date(name = "Month") +
+  scale_y_continuous(name = "Monthly Cumulative Flux [in]") +
+  labs(title = "Cumulative Monthly ET (color lines) and Precip + Reported Irr (black dots)") +
+  theme(legend.position = "bottom")
+ggsave("plots/FieldData_MonthlyCumulativeP+Irr.png", width = 150, height = 150, units = "mm")
 
 # scatterplot comparison
 ggplot(df_long, aes(x = irrEst_mm/25.4, y = irrigation_mm/25.4, color = FieldID)) +
