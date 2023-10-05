b2a70da72c19b86527aaafc8f2b6e74a09c3c15e
Sam Zipper <samzipper@ku.edu>
2023-10-05 11:58:41 -0500

    Minor tweak - bring from laptop to desktop

--- code/FieldData-Annual_02_EstimateIrrigation.R
+++ code/FieldData-Annual_02_EstimateIrrigation.R
@@ -38,31 +38,53 @@ df_corn <- subset(df_long, cropType %in% c("Corn", "Corn-Grain", "Corn-Corn"))
 ggplot(df_corn, aes(x = irrEst_mm/25.4, y = irrigation_mm/25.4)) +
   geom_abline(color = col.gray) +
   geom_point(aes(color = str_sub(FieldID, 1, 2))) +
-  stat_smooth(method = "lm", color = "black", linetype = "dashed", SE = F) +
+  #stat_smooth(method = "lm", color = "black", linetype = "dashed") +
   scale_x_continuous(name = "Estimated Irrigation [in]") +
   scale_y_continuous(name = "Reported Irrigation [in]") +
+  coord_equal() +
   scale_color_manual(name = "Region", values = c("NC" = col.cat.blu, "WC" = col.cat.yel, "SW" = col.cat.red)) +
-  facet_wrap(~Algorithm, labeller = as_labeller(labs_algorithms)) +
-  labs(title = "Annual Reported vs. Estimated Irrigation (ET - P), Corn Only") +
-  theme(legend.position = "bottom")
-ggsave(file.path("plots", "FieldData-Annual_Scatterplot.png"),
-       width = 150, height = 150, units = "mm")
+  facet_wrap(~Algorithm, nrow = 2, labeller = as_labeller(labs_algorithms))
+ggsave(file.path("plots", "FieldData-Annual_ScatterAnnual.png"),
+       width = 190, height = 75, units = "mm")
 
-# plot cumsum - first choose only sites with >= 4 yrs data
-fields_multiyear <- 
+# plot cumsum - first choose only sites with >= 4 yrs (50%) data
+fields_multiyr <- 
   df_all |> 
   group_by(FieldID) |> 
   summarize(n_years = n()) |> 
   subset(n_years >= 4)
 
-df_6yr <- subset(df_long_cumirr, FieldID %in% fields_multiyear$FieldID & FieldID != "WC13") # no OpenET data for WC13
-ggplot(data = df_6yr, aes(x = Year)) +
+df_multiyr <- subset(df_long_cumirr, FieldID %in% fields_multiyr$FieldID & FieldID != "WC13") # no OpenET data for WC13
+ggplot(data = df_multiyr, aes(x = Year)) +
   geom_line(aes(y = irrEst_mm_cumsum/25.4, color = Algorithm)) +
   geom_point(aes(y = irrigation_mm_cumsum/25.4), shape = 1, color = "black") +
-  facet_wrap(~FieldID, ncol = 9) +
+  facet_wrap(~FieldID, nrow = 4) +
   scale_x_continuous(breaks = seq(2016, 2022, 3)) +
   scale_y_continuous(name = "Cumulative Irrigation [in]") +
   scale_color_manual(values = pal_algorithms, labels = labs_algorithms) +
   theme(legend.position = "bottom")
 ggsave(file.path("plots", "FieldData-Annual_Cumulative.png"),
-       width = 150, height = 150, units = "mm")
+       width = 150, height = 210, units = "mm")
+
+# average across cumsum period
+df_multiyr_mean <-
+  df_multiyr |> 
+  group_by(FieldID, Algorithm) |> 
+  filter(Year == max(Year)) |> 
+  left_join(fields_multiyr, by = "FieldID") |> 
+  mutate(irrigation_mm_mean = irrigation_mm_cumsum/n_years,
+         irrEst_mm_mean = irrEst_mm_cumsum/n_years)
+
+ggplot(data = df_multiyr_mean, aes(x = irrEst_mm_mean/25.4, y = irrigation_mm_mean/25.4)) +
+  geom_abline(intercept = 0, slope = 1, color = col.gray) +
+  geom_point(aes(color = factor(n_years))) +
+  facet_wrap(~Algorithm, nrow = 2, labeller = as_labeller(labs_algorithms)) +
+  stat_smooth(method = "lm") +
+  scale_x_continuous(name = "Estimated Irrigation [in/yr], Multi-Year Mean",
+                     limits = c(0, 31)) +
+  scale_y_continuous(name = "Reported Irrigation [in/yr], Multi-Year Mean",
+                     limits = c(0, 31)) +
+  coord_equal() +
+  scale_color_manual(name = "# of Years", values = c("#feebe2", "#fbb4b9", "#f768a1", "#ae017e"))
+ggsave(file.path("plots", "FieldData-Annual_ScatterMean.png"),
+       width = 190, height = 100, units = "mm")
